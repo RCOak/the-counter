e6a448df22e5a7bc813259998afaf6ce806d7b46
RCOak <coliveirarenato90@gmail.com>
2022-12-15 19:12:11 +0000

    trying

---
+++ src/js/component/icons.js
@@ -0,0 +1,6 @@
+import { library, icon } from '@fortawesome/fontawesome-svg-core'
+import { faClock } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faClock)
+
+const clock = icon({ prefix: 'far', iconName: 'clock' })
--- src/js/index.js
+++ src/js/index.js
@@ -2,11 +2,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+
+
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
 //import your own components
-import Home from "./component/home.jsx";
+import "./component/icons"
+
+function simplecounter(props){
+    return <div className="thecounter">
+        <div className="icone"><i className="fa-regular fa-clock"></i></div>
+        <div className="seis">0</div>
+        <div className="cinco">0</div>
+        <div className="quatro">0</div>
+        <div className="tres">0</div>
+        <div className="dois">0</div>
+        <div className="um">0</div>
+    </div>;
+}
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<simplecounter />, document.querySelector("#app"));
