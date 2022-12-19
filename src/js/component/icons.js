974871c6ba242079fd75fcfd511ad969295ceb2a
RCOak <coliveirarenato90@gmail.com>
2022-12-19 20:32:22 +0000

    done

--- src/js/component/icons.js
+++
@@ -1,6 +0,0 @@
-import { library, icon } from '@fortawesome/fontawesome-svg-core'
-import { faClock } from '@fortawesome/free-solid-svg-icons'
-
-library.add(faClock)
-
-const clock = icon({ prefix: 'fa', iconName: 'clock' })
--- src/js/index.js
+++ src/js/index.js
@@ -8,7 +8,18 @@ import SimpleCounter from "./component/SimpleCounter.js"
 import "../styles/index.css";
 
 //import your own components
-import "./component/icons"
+let counter = 0;
+setInterval(function(){
+    const seis = Math.floor(counter/100000);
+    const cinco = Math.floor(counter/10000);
+    const quatro = Math.floor(counter/1000);
+    const tres = Math.floor(counter/100);
+    const dois = Math.floor(counter/10);
+    const um = Math.floor(counter/1);
+    console.log(seis, cinco, quatro, tres, dois, um)
+    counter++;
 
+    ReactDOM.render(<SimpleCounter digitoUm={um} digitoDois={dois} digitoTres={tres} digitoQuatro={quatro} digitoCinco={cinco} digitoSeis={seis} />, document.querySelector("#app"));
+},1000);
 //render your react application
-ReactDOM.render(<SimpleCounter />, document.querySelector("#app"));
+
